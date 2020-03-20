d52284cb1911fe6399ca2a0277f3b04ec47c2629
Marius Ambrazevicius <work.marius.ambrazevicius@gmail.com>
2020-03-20 16:38:23 +0200

    Symfony update fix

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -3,13 +3,30 @@
 namespace PlumTreeSystems\UserBundle\Command;
 
 use PlumTreeSystems\UserBundle\Exception\UserException;
+use PlumTreeSystems\UserBundle\Service\UserManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
-class CreateUserCommand extends ContainerAwareCommand
+class CreateUserCommand extends Command
 {
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+
+    /**
+     * CreateUserCommand constructor.
+     * @param UserManager $userManager
+     */
+    public function __construct(UserManager $userManager)
+    {
+        parent::__construct();
+        $this->userManager = $userManager;
+    }
+
     protected function configure()
     {
         $this
@@ -28,7 +45,7 @@ class CreateUserCommand extends ContainerAwareCommand
         $questionEmail = new Question('Enter your Email: ');
         $questionPassword = new Question('Enter your Password: ');
         $questionRole = new Question('Enter defined role (role example: ROLE_ADMIN)');
-        $manager = $this->getContainer()->get('pts_user.manager');
+        $manager = $this->userManager;
 
         $helper = $this->getHelper('question');
 
